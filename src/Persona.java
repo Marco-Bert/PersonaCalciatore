6723a7d9d1ffa5601ec6eaf819b9a8136c826460
Marco-Bert <marco.bertagnolli@buonarroti.tn.it>
2023-10-16 08:37:12 +0200

    feat: getter, setter e clone

--- src/Persona.java
+++ src/Persona.java
@@ -13,7 +13,21 @@ public class Persona {
         this.dataNascita = persona.dataNascita;
         this.sesso = persona.sesso;
     }
-
+    public String getNome(){
+        return this.nome;
+    }
+    public String getDataNascita(){
+        return dataNascita;
+    }
+    public String getSesso(){
+        return this.sesso;
+    }
+    public void setSesso(String sesso){
+        this.sesso = sesso;
+    }
+    public Object clone(){
+        return (Object) new Persona(this.nome, this.dataNascita, this.sesso);
+    }
     public String toString(){
         return "{\"nome\": \"" + this.nome + "\", \"dataNascita\": \"" + this.dataNascita + "\", \"sesso\": \"" + this.sesso + "\"}";
     }
